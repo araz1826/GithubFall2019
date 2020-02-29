9d3eab47bcd244fcd2928334f0db1876452b27b5
araz1826 <arazismailov@yahoo.com>
2020-02-29 11:45:11 -0700

    my first commit

---
+++ src/day1/StringPractice.java
@@ -0,0 +1,19 @@
+package day1;
+                                 //how to send it to github? how to find copy on github
+//Local repository: we create it for specific project  (git init: it means initialize) .
+// remote repository: we use github
+
+//git init:once for each project at the very beginning
+//git status: to see what files can be committed
+//.gitignore file: to permanently exclude the certain files/packages from commit we use
+//.idea  and *.iml - intellij
+//right click create a file name it gitignore
+//StringPractice: .idea and *.idea
+// use arrow up or down to see your previous command
+
+public class StringPractice {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+}
