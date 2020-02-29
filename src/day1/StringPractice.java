9b20da0c296cc72843622ab05b61dd89c76ba65c
araz1826 <arazismailov@yahoo.com>
2020-02-29 13:27:00 -0700

    commit message

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,15 +5,25 @@ package day1;
 
 //git init:once for each project at the very beginning
 //git status: to see what files can be committed
-//.gitignore file: to permanently exclude the certain files/packages from commit we use
+//.gitignore file: to permanently exclude the certain files/packages from commit we use . right click and create a file name it .gitignore
 //.idea  and *.iml - intellij
 //right click create a file name it gitignore
 //StringPractice: .idea and *.idea
 // use arrow up or down to see your previous command
 
+//git add .  after this do "git status" then u will see all turned into green
+//git commit -m "my first commit"
+
+
+//git remote add origin https://github.com/araz1826/GithubFall2019.git  // we get this from github after creating repository and add to terminal
+
+//git push -u origin master
+
+//git status// when u add something put this in terminal and see what MODIFIED(red color)
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(" new commit");
 
     }
 }
